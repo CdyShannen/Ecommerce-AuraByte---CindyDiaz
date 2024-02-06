99b781c45e6c364b388e337c0946cc86327fa084
“Cindy <cindysdiaza@gmail.com>
2024-02-05 21:15:06 -0300

    Modificacion css

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,40 +1,39 @@
 import CartWidget from '../CartWidget/CartWidget';
 import './NavBar.css';
-import {Link, NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <header>
-      <Link to="/">
-        <img className='header__logo' src="../public/img/logo.jpg" alt="logo" />
-      </Link>
+        <Link to="/">
+          <img className='header__logo' src="../public/img/logo.jpg" alt="logo" />
+        </Link>
 
         <nav>
-            <ul>
+          <ul>
 
-                <li>
-                  <NavLink to="/categoria/1" className="header__categorias">
-                    iPhone
-                  </NavLink>
-                </li>
+            <li>
+              <NavLink to="/categoria/1" className="header__categorias">
+                iPhone
+              </NavLink>
+            </li>
 
-                <li>
-                  <NavLink to="/categoria/2" className="header__categorias">
-                    Apple Watch
-                  </NavLink>
-                </li>
+            <li>
+              <NavLink to="/categoria/2" className="header__categorias">
+                Apple Watch
+              </NavLink>
+            </li>
 
-                <li>
-                  <NavLink to="/categoria/3" className="header__categorias">
-                    Accesorios
-                  </NavLink>
-                </li>
+            <li>
+              <NavLink to="/categoria/3" className="header__categorias">
+                Accesorios
+              </NavLink>
+            </li>
 
-            </ul>
+          </ul>
         </nav>
-        
-        <CartWidget/>
 
+        <CartWidget />
     </header>
   )
 }
